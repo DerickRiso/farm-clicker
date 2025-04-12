c6521b45229cc95ba8293a62a31bfee39905630f
Derick Darlan <derickdarlan304@gmail.com>
2025-04-12 10:38:15 -0300

    Power Up de adicionar um click finalmente adicionado, depois de um bug infernal

--- js/clique.js
+++ js/clique.js
@@ -1,23 +1,21 @@
 const numMilho = document.getElementById("milho");
 const clicador = document.getElementById("clickArea");
-var cont = 0;
-var escalar = 1;
-var milho = 0;
+var contagemMilho = 0;      // Variável que intermedia as operações com o milho do player
+var cont = 0;               // Contagem de clicks físicos
+var clique = 1;             // Numero de milho por click
+var escalar = 1;            // Escalar pra multiplicar (power up)
+var milhoPlayer = 0;        // Quantidade atual de milho do jogador
+
+
 
 // MILHO
 function gerarMilhoJogador() {
-    milho = (escalar * cont);  
+    contagemMilho = clique;
 }
 
-function contar() {
-    cont++
-    gerarMilhoJogador();
-
-    numMilho.innerHTML = `Milho: ${milho}`;
-
-    const instancia = criarInstancia();
-    destruirInstancia(instancia);
-}   
+function atualizar() {
+    numMilho.innerHTML = `Milho: ${milhoPlayer}`;
+}
 
 // INSTANCIAS
 function criarInstancia() {
@@ -34,4 +32,30 @@ function destruirInstancia(instancia) {
         instancia.remove();
     }, 1500);
 }
+
+// UPGRADES E COMPRA
+function comprar(preco) {
+    milhoPlayer = milhoPlayer - preco;
+    atualizar();
+}
+
+function adicionarClique() {
+    var valor = 1
+    if(milhoPlayer >= valor) {
+        comprar(valor);
+        clique++;
+    }
+}
+
+function contar() {
+    cont++;
+    gerarMilhoJogador();
+    milhoPlayer += contagemMilho;
+    atualizar();
+
+    const instancia = criarInstancia();
+    destruirInstancia(instancia);
+} 
+
+
 
