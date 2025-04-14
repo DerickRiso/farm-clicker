20eef567d61d1b97f089e6b3f55a2d3c70920171
Derick Darlan <derickdarlan304@gmail.com>
2025-04-14 16:25:05 -0300

    Dois upgrades adicionados ao jogo

--- js/clique.js
+++ js/clique.js
@@ -1,9 +1,9 @@
 const numMilho = document.getElementById("milho");
 const clicador = document.getElementById("clickArea");
+const clic = document.getElementById("clicador");
 var contagemMilho = 0;      // Variável que intermedia as operações com o milho
 var cont = 0;               // Contagem de clicks físicos (vou usar mais tarde)
 var clique = 1;             // Numero de milho por click
-var escalar = 1;            // Escalar pra multiplicar (power up)
 var milhoPlayer = 0;        // Quantidade atual de milho do jogador
 
 // MILHO
@@ -38,10 +38,38 @@ function comprar(preco) {
 }
 
 function adicionarClique() {
-    var valor = 1
+    var valor = 1;
     if(milhoPlayer >= valor) {
         comprar(valor);
         clique++;
+    }
+}
+
+function bonusMilho() {
+    let bonus = 100;
+    var valor = 1;
+    if(milhoPlayer >= valor) {
+        comprar(valor);
+        setInterval(() => {
+            milhoPlayer += bonus;
+            atualizar();
+        }, 30 * 1000)
+    }
+}
+
+
+function autoclick() {
+    var valor = 1;
+    if(milhoPlayer >= valor) {
+        comprar(valor);
+        setInterval(()=> {
+            contar();
+            atualizar();
+            setInterval(() => {clic.classList.add('autoclick')}, 1000);
+            setInterval(() => {clic.classList.remove('autoclick')}, 2000);
+        }, 2000);
+        
+        
     }
 }
 
